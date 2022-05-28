d4d43e1b5b53e8702a2240d22ddb9e5f08a0e09d
kvn-mz <kevinmezamc@gmail.com>
2022-05-27 20:46:07 -0600

    Python 0.0.0.3.1

--- Tarea 28-05-2022/Semanal_Diagram_01.py
+++ Tarea 28-05-2022/Semanal_Diagram_01.py
@@ -1,13 +1,96 @@
-lado1 = int(input("Ingrese el lado A :"))
-lado2 = int(input("Ingrese el lado B :"))
-lado3 = int(input("Ingrese el lado C :"))
-
-sumaLado12 = lado1+lado2
-sumaLado13 = lado1+lado3
-sumaLado23 = lado2+lado3
-
-if (sumaLado12 > lado3) & (sumaLado13 > lado2) & (sumaLado23 > lado1):
-    print("Es un triangulo")
-
+flag=0
+day=int(input("Ingrese el dia: "))
+month=int(input("Ingrese el mes: "))
+year=int(input("Ingrese el annio: "))
+if(year%4==0):
+    if(year%100!=0 or year%400==0):
+        leap=1
+    else:
+        leap=0
 else:
-    print("No es un triangulo")
+    leap=0
+if(leap==1):
+    if(month==2):
+        if(day>=1 and day<=28):
+            day=day+1
+            #day+=1 es lo mismo que lin 15
+        #elif(day==29):
+        else:
+            if(day==29):
+                day=1
+                month+=1
+            else:
+                print("fecha invalida, dia incorrecto")
+                flag=1
+    else:
+        if(month==4 or month==6 or month==9 or month==11):
+            if (day >= 1 and day <= 29):
+                day = day + 1
+            else:
+                if (day == 30):
+                    day = 1
+                    month += 1
+                else:
+                    print("fecha invalida, dia incorrecto")
+                    flag = 1
+        else:
+            if(month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12):
+                if(month==12 and day==31):
+                    year+=1
+                    month=1
+                    day=1
+                else:
+                    if (day >= 1 and day <= 30):
+                        day = day + 1
+                    else:
+                        if (day == 31):
+                            day = 1
+                            month += 1
+                        else:
+                            print("fecha invalida, dia incorrecto")
+                            flag = 1
+            else:print("fecha incorrecta, mes invalido")
+            flag=1
+else:
+    if (month == 2):
+        if (day >= 1 and day <= 27):
+            day = day + 1
+        else:
+            if (day == 28):
+                day = 1
+                month += 1
+            else:
+                print("fecha invalida, dia incorrecto")
+                flag = 1
+    else:
+        if (month == 4 or month == 6 or month == 9 or month == 11):
+            if (day >= 1 and day <= 29):
+                day = day + 1
+            else:
+                if (day == 30):
+                    day = 1
+                    month += 1
+                else:
+                    print("fecha invalida, dia incorrecto")
+                    flag = 1
+        else:
+            if (month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12):
+                if (month == 12 and day == 31):
+                    year += 1
+                    month = 1
+                    day = 1
+                else:
+                    if (day >= 1 and day <= 30):
+                        day = day + 1
+                    else:
+                        if (day == 31):
+                            day = 1
+                            month += 1
+                        else:
+                            print("fecha invalida, dia incorrecto")
+                            flag = 1
+            else:
+                print("fecha incorrecta, mes invalido")
+                flag = 1
+if(flag==0):
+    print(day,"-",month,"-",year)
--- Tarea 28-05-2022/Semanal_Diagram_02.py
+++ Tarea 28-05-2022/Semanal_Diagram_02.py
@@ -1,18 +1,13 @@
-name = input("Digite el nombre del cliente: ")
-age = int(input("Digite los años de fidelidad: "))
-pay = float(input("Digite el monto a recargar: "))
+lado1 = int(input("Ingrese el lado A :"))
+lado2 = int(input("Ingrese el lado B :"))
+lado3 = int(input("Ingrese el lado C :"))
 
-if (1 < age < 3) & (2000 <= pay < 3800):
-    total = pay * 2
-    print("Estimado(a) cliente ", name, " su recargada es por un monto total de ", total,
-          " colones, se le ha aplicado la promoción de recarga doble")
-elif (3 <= age < 5) & (3800 <= pay <= 5000):
-    total = pay * 3
-    print("Estimado(a) cliente ", name, " su recargada es por un monto total de ", total,
-              ' colones, se le ha aplicado la promoción de recarga triple')
-elif (age >= 5):
-    total = pay * 3
-    print("Estimado(a) cliente ", name, " su recargada es por un monto total de ", total,
-          ' colones, se le ha aplicado la promoción de recarga triple')
+sumaLado12 = lado1+lado2
+sumaLado13 = lado1+lado3
+sumaLado23 = lado2+lado3
+
+if (sumaLado12 > lado3) & (sumaLado13 > lado2) & (sumaLado23 > lado1):
+    print("Es un triangulo")
+
 else:
-    print("Estimado cliente", name, "el monto de su recarga es de: ", pay)
+    print("No es un triangulo")
